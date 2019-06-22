712d386402e920ed4144f9451b163ea83cf0b608
chinnari607 <42085389+chinnari607@users.noreply.github.com>
2019-06-22 11:38:34 -0700

    Update _bank_prediction.py

--- _bank_prediction.py
+++ _bank_prediction.py
@@ -6,15 +6,15 @@ names=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","
 data=pandas.read_csv(path,names=names)
 x=data.iloc[ :,:24]
 y=data.iloc[ :,24]
-'''
-correlations=data.corr()
-#print(correlations)
 
+###finding correlation
+
+correlations=data.corr()
+print(correlations)
 fig=plt.figure()
 ax=fig.add_subplot(111)
 cax=ax.matshow(correlations,vmax=1,vmin=-1)
 plt.show()
-
 
 fig.colorbar(cax)
 ticks=numpy.arange(0,9,1)
@@ -23,48 +23,47 @@ ax.set_yticks(ticks)
 ax.set_xticklabels(names)
 ax.set_yticklabels(names)
 plt.show()
+
+###preprocessing of dataset
+
 from sklearn import preprocessing
 le=preprocessing.LabelEncoder()
 data["class"]=le.fit_transform(data["class"])
-print(data)'''
+print(data)
 
+###training and testing of dataset
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=1)
 
 ###knn
+
 from sklearn.neighbors import KNeighborsClassifier
 model=KNeighborsClassifier(n_neighbors=5)
 model.fit(x_train,y_train)
-
 y_pred=model.predict(x_test)
 print(y_pred)
-
-
 from sklearn.metrics import accuracy_score
 print("accuracy of knn :",accuracy_score(y_test,y_pred)*100)
 
 ###naive bayes
 
 from sklearn.naive_bayes import GaussianNB
-
 model = GaussianNB()
-
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
 print(y_pred)
-
 from sklearn.metrics import accuracy_score
 print("accuracy of nb:",accuracy_score(y_test,y_pred)*100)
 
 
 ###random forest
+
 from sklearn.ensemble import RandomForestClassifier
 model=RandomForestClassifier()
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
 print(y_pred)
-
 from sklearn.metrics import accuracy_score
 print("accuracy of rf:",accuracy_score(y_test,y_pred)*100)
 
@@ -76,27 +75,21 @@ model=DecisionTreeClassifier("entropy")
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
 print(y_pred)
-
 from sklearn.metrics import accuracy_score
 print("accuracy of dt:",accuracy_score(y_test,y_pred)*100)
+
+
 ###logestics
+
 from sklearn.linear_model import LogisticRegression
 model=LogisticRegression()
 model.fit(x_train,y_train)
-
 y_pred=model.predict(x_test)
 print(y_pred)
-
 from sklearn.metrics import accuracy_score
 print("accuracy of logestics:",accuracy_score(y_test,y_pred)*100)
 
-'''#SVM
-from sklearn.svm import SVC
-model_svm=SVC(kernel="linear")
-model_svm.fit(x_train,y_train)
-y_pred_svm=model_svm.predict(x_test)
 
-print(y_pred_svm)
-from sklearn.metrics import accuracy_score
-print("accuracy for svm",accuracy_score(y_pred_svm,y_test)*100)'''
+
+
 
