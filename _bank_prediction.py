bb6a2080540e3ccb5151eee20ea12d646d2d0dfd
chinnari607 <42085389+chinnari607@users.noreply.github.com>
2019-06-22 23:36:50 +0530

    Add files via upload
    This is about whether the person will pay the credit card bill or not. Based on the training of the dataset the result is predicted. The classification approach is applied for this problem as it is of 2-way classification business problem. For this business problem, the random forest algorithm is most suitable and it gives the accuracy of 80.25%.

---
+++ _bank_prediction.py
@@ -0,0 +1,102 @@
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+path="D:\machine_learning\_bank_credit.csv"
+names=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"]
+data=pandas.read_csv(path,names=names)
+x=data.iloc[ :,:24]
+y=data.iloc[ :,24]
+'''
+correlations=data.corr()
+#print(correlations)
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+cax=ax.matshow(correlations,vmax=1,vmin=-1)
+plt.show()
+
+
+fig.colorbar(cax)
+ticks=numpy.arange(0,9,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(names)
+ax.set_yticklabels(names)
+plt.show()
+from sklearn import preprocessing
+le=preprocessing.LabelEncoder()
+data["class"]=le.fit_transform(data["class"])
+print(data)'''
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=1)
+
+###knn
+from sklearn.neighbors import KNeighborsClassifier
+model=KNeighborsClassifier(n_neighbors=5)
+model.fit(x_train,y_train)
+
+y_pred=model.predict(x_test)
+print(y_pred)
+
+
+from sklearn.metrics import accuracy_score
+print("accuracy of knn :",accuracy_score(y_test,y_pred)*100)
+
+###naive bayes
+
+from sklearn.naive_bayes import GaussianNB
+
+model = GaussianNB()
+
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+print(y_pred)
+
+from sklearn.metrics import accuracy_score
+print("accuracy of nb:",accuracy_score(y_test,y_pred)*100)
+
+
+###random forest
+from sklearn.ensemble import RandomForestClassifier
+model=RandomForestClassifier()
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+print(y_pred)
+
+from sklearn.metrics import accuracy_score
+print("accuracy of rf:",accuracy_score(y_test,y_pred)*100)
+
+###decision tree
+
+from sklearn.tree import DecisionTreeClassifier
+#model=DecisionTreeClassifier()
+model=DecisionTreeClassifier("entropy")
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+print(y_pred)
+
+from sklearn.metrics import accuracy_score
+print("accuracy of dt:",accuracy_score(y_test,y_pred)*100)
+###logestics
+from sklearn.linear_model import LogisticRegression
+model=LogisticRegression()
+model.fit(x_train,y_train)
+
+y_pred=model.predict(x_test)
+print(y_pred)
+
+from sklearn.metrics import accuracy_score
+print("accuracy of logestics:",accuracy_score(y_test,y_pred)*100)
+
+'''#SVM
+from sklearn.svm import SVC
+model_svm=SVC(kernel="linear")
+model_svm.fit(x_train,y_train)
+y_pred_svm=model_svm.predict(x_test)
+
+print(y_pred_svm)
+from sklearn.metrics import accuracy_score
+print("accuracy for svm",accuracy_score(y_pred_svm,y_test)*100)'''
+
